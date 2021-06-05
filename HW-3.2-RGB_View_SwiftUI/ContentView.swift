8045fbacb9655d4519f22ac7567d796eeae6466d
Nikita Kelevra <nonamerz5@mail.ru>
2021-06-05 08:07:26 +0300

    Initial Commit

---
+++ HW-3.2-RGB_View_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW-3.2-RGB_View_SwiftUI
+//
+//  Created by Сперанский Никита on 05.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW-3.2-RGB_View_SwiftUI/HW_3_2_RGB_View_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_2_RGB_View_SwiftUIApp.swift
+//  HW-3.2-RGB_View_SwiftUI
+//
+//  Created by Сперанский Никита on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_2_RGB_View_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
