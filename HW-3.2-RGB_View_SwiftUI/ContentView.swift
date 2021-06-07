27bb069b2883dcb512862914a0b438ea3b57402f
Nikita Kelevra <nonamerz5@mail.ru>
2021-06-07 15:38:46 +0300

    RGB Color Theme

--- HW-3.2-RGB_View_SwiftUI/ContentView.swift
+++ HW-3.2-RGB_View_SwiftUI/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var redLightValueSlider = Double.random(in: 0...255)
+    @State private var greenLightValueSlider = Double.random(in: 0...255)
+    @State private var blueLightValueSlider = Double.random(in: 0...255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MainView(redLightValueSlider: $redLightValueSlider, greenLightValueSlider: $greenLightValueSlider, blueLightValueSlider: $blueLightValueSlider)
+            
+            SliderBar(value: $redLightValueSlider, sliderColor: .red)
+            SliderBar(value: $greenLightValueSlider, sliderColor: .green)
+            SliderBar(value: $blueLightValueSlider, sliderColor: .blue)
+        }
+        .padding(.vertical)
+        .background(Color(.purple).opacity(0.5))
+        .cornerRadius(20)
+        .overlay(RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color(.black),
+                            lineWidth: 2))
+        Spacer()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct MainView: View {
+    
+    @Binding var redLightValueSlider: Double
+    @Binding var greenLightValueSlider: Double
+    @Binding var blueLightValueSlider: Double
+        
+    var body: some View {
+        Text(" ")
+            .padding(.top, 200)
+            .padding(.horizontal, 200)
+            .background(Color(red: redLightValueSlider,
+                              green: greenLightValueSlider,
+                              blue: blueLightValueSlider,
+                              opacity: 1))
+            .cornerRadius(20)
+            .overlay(RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color(.black),
+                                lineWidth: 2)
+            )
     }
 }
---
+++ HW-3.2-RGB_View_SwiftUI/SliderBar.swift
@@ -0,0 +1,83 @@
+//
+//  SliderBar.swift
+//  HW-3.2-RGB_View_SwiftUI
+//
+//  Created by Сперанский Никита on 05.06.2021.
+//
+
+import SwiftUI
+
+struct SliderBar: View {
+    
+//    @State private var value = Double.random(in: 0...255)
+    @Binding var value: Double
+    
+    var sliderColor: UIColor = .black
+    
+    var body: some View {
+        
+        HStack {
+            Text("\(lround(value))")
+                .frame(maxWidth: 50)
+                .padding(.leading, 10)
+            Slider(value: $value, in: 0...255, step: 1)
+            EditView(value: $value)
+        }
+        .accentColor(Color(sliderColor))
+        .background(Color.black.opacity(0.2))
+        .cornerRadius(30)
+    }
+}
+
+struct SliderBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderBar(value: .constant(155), sliderColor: .blue)
+    }
+}
+
+struct EditView: View {
+    
+    @State private var alertPressented: Bool = false
+    
+    @Binding var value: Double
+    
+    var body: some View {
+        
+        let valueFormatter = Binding<String>(
+            get: {String(format: "%.00f", Double(self.value))},
+            set: {
+                if let someValue = NumberFormatter().number(from: $0) {
+                    self.value = someValue.doubleValue
+                }
+            }
+        )
+        
+        TextField(
+            "\(lround(value))", text: valueFormatter)
+            
+            .alert(isPresented: $alertPressented,
+                   content: {
+                    Alert(
+                        title: Text("Введенное число должно быть в диапазоне от 0 до 255")
+                    )
+                   })
+            
+            .frame(maxWidth: 45)
+            .scaledToFit()
+            .keyboardType(.numberPad)
+            .padding(5)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .disableAutocorrection(true)
+            .padding(.trailing, 10)
+            .onChange(of: value) {
+                changedValue in
+                if Double(changedValue) < 0 || Double(changedValue) > 255 {
+                    value = 255
+                    alertPressented.toggle()
+                    print("Ups! Check the Value!!!!!!!")
+                    return
+                }
+                
+            }
+    }
+}
